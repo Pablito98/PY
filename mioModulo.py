8f58fcf86057de23c783e0714d75beafe05ad413
Pablito98 <paolofidanza16@gmail.com>
2025-01-09 13:31:13 +0100

    moduli

---
+++ mioModulo.py
@@ -0,0 +1,9 @@
+def saluta(nome):
+    print("ciao "+nome)
+
+persona1={
+    "nome" : "luca",
+    "cognome" : "rossi"
+}
+
+parteDelModulo=[23,25]
---
+++ moduli.py
@@ -0,0 +1,22 @@
+#importare con alias
+import mioModulo as em
+import platform
+import math
+#importare solo una parte del modulo
+from mioModulo import parteDelModulo
+
+#con funzione
+em.saluta("luca")
+#con variabile
+x=em.persona1["nome"]
+em.saluta(x)
+
+
+#platform è un modulo di py
+y = platform.system()
+print(y)
+
+#math è un modulo di py
+print(math.floor(2.90))
+#con la funzione dir(math) vedo tutti i metodi del modulo
+print(parteDelModulo)
